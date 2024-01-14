f13a7705aab63a6b84e903787a0f9ac1f6b5c052
Mehedi Ahsan <mdmehedicse01@gmail.com>
2024-01-15 00:44:48 +0600

    show post on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,42 @@
 
+import { useEffect } from 'react';
+import loadPostData from '../redux/thunk/loadPostData';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
+    const posts = useSelector((state) => state.posts);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(loadPostData())
+    })
     return (
-        <div>
-            sdf
+        <section className="py-16">
+        <div className="max-w-screen-xl mx-auto px-4 md:px-8">
+            <ul className="grid gap-10 sm:grid-cols-2 lg:grid-cols-3">
+                {
+                    posts?.map((item, idx) => (
+                        <li key={idx} className="border rounded-lg">
+                            <div className="flex items-start justify-between p-4">
+                                <div className="space-y-2">
+                                    {item.icon}
+                                    <h4 className="text-blue-400 font-semibold">{item.title.slice(0,20)}</h4>
+                                    <p className="text-gray-300 text-sm">{item.body.slice(0.100)}...</p>
+                                </div>
+                            </div>
+                            <div className="py-3 px-4 border-t text-right">
+                                <Link to={`post/${item.id}`} className="text-blue-400 hover:text-indigo-500 text-sm font-medium">
+                                    View Details
+                                </Link>
+                            </div>
+                        </li>
+                    ))
+                }
+            </ul>
         </div>
+    </section>
     );
 };
 
